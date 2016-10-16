31ad96515ae6fdc5bdcf4019bf63a4c61daad6a4
yn <y_nkzw@ifty.ne.jp>
2016-10-16 12:24:25 +0900

    first commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,9 @@
+class TopController < ApplicationController
+  def index
+    @articles = Article.open.order(released_at: :desc).limit(5)
+  end
+
+  def about
+
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Asagao
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.action_controller.permit_all_parameters = true
+  end
+end
